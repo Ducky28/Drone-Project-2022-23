6b860398f09aefc1e585b804648988cdb892e335
Ben Phan <66501019+LiterallyCanada312@users.noreply.github.com>
2022-11-13 13:15:44 -0500

    Add files via upload
    made a face detector

---
+++ FaceDetection/main.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+faceData = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    ret,frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    targets = faceData.detectMultiScale(gray, minSize=(10,10))
+
+    amountFound = len(targets)
+
+    if amountFound != 0:
+        for(x, y, width, height) in targets:
+            cv.rectangle(frame, (x, y), 
+                      (x + height, y + width), 
+                      (0, 255, 0), 5)
+    
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
+
+
