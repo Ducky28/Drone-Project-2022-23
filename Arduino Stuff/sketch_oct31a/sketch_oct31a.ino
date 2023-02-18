a13bc40c4d69856169e836af323841f1c986472d
Ben <whippedcreamtoast@gmail.com>
2023-02-18 10:21:38 -0500

    reworked since we control servo w betaflight

--- Arduino Stuff/sketch_oct31a/sketch_oct31a.ino
+++ Arduino Stuff/sketch_oct31a/sketch_oct31a.ino
@@ -1,49 +1,48 @@
-#include <Servo.h>
-
-Servo servo;
-
-int servoPin = 9;
 
 int PWMController1Pin = 3;
 int PWMController2Pin = 5;
 
-int servoStartPos = 0;
-int servoReadyPos = 180; 
-int servoFirePos = 0;
-
-#define SPIN_MOTORS 0
-#define PREP_SERVO 1
+#define STOP 0
+#define SPIN_MOTORS 1
 #define FIRE 2
 #define RELOAD 3
+
+#define SHOOTER_DELAY 1000
 
 int shooterStage = 0; 
 
 void setup(){
+  Serial.begin(115200);
+
   pinMode(PWMController1Pin, OUTPUT);
   pinMode(PWMController2Pin, OUTPUT);
-  servo.attach(servoPin);
+
+  Serial.println("FINISHED INIT, BEGINNING LOOP");
 }
 
 void loop(){
   switch(shooterStage){
+
+    case STOP:
+
+      analogWrite(PWMController1Pin, LOW);
+      analogWrite(PWMController2Pin, LOW);
+      break;
 
     case SPIN_MOTORS:
       analogWrite(PWMController1Pin, HIGH);
       analogWrite(PWMController2Pin, HIGH);
       break;
 
-    case PREP_SERVO:
-      servo.write(servoReadyPos);
-      break
-
     case FIRE:
-      servo.write(servoFirePos);
+      delayMicroseconds(SHOOTER_DELAY);
       break;
 
     case RELOAD:
-      servo.write(servoStartPos);
-      analogWrite(PWMController1Pin);
-      analogWrite(PWMController2Pin);
+
+      analogWrite(PWMController1Pin, LOW);
+      analogWrite(PWMController2Pin, LOW);
+      break;
 
   }
   
