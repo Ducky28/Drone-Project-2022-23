3be8bd7ab23876460cc385c685991d55b4b74b76
Ben Phan <whippedcreamtoast@gmail.com>
2023-02-21 14:35:23 -0500

    added script to test pwm motors

--- Arduino Stuff/sketch_oct31a/sketch_oct31a.ino
+++ Arduino Stuff/sketch_oct31a/sketch_oct31a.ino
@@ -1,50 +1,12 @@
-#include <Servo.h>
-
-Servo servo;
-
-int servoPin = 9;
-
-int PWMController1Pin = 3;
-int PWMController2Pin = 5;
-
-int servoStartPos = 0;
-int servoReadyPos = 180; 
-int servoFirePos = 0;
-
-#define SPIN_MOTORS 0
-#define PREP_SERVO 1
-#define FIRE 2
-#define RELOAD 3
-
-int shooterStage = 0; 
+#define motor1Pin 1
+#define motor2Pin 7
 
 void setup(){
-  pinMode(PWMController1Pin, OUTPUT);
-  pinMode(PWMController2Pin, OUTPUT);
-  servo.attach(servoPin);
+    pinMode(motor1Pin, OUTPUT);
+    pinMode(motor2Pin, OUTPUT);
 }
 
 void loop(){
-  switch(shooterStage){
-
-    case SPIN_MOTORS:
-      analogWrite(PWMController1Pin, HIGH);
-      analogWrite(PWMController2Pin, HIGH);
-      break;
-
-    case PREP_SERVO:
-      servo.write(servoReadyPos);
-      break
-
-    case FIRE:
-      servo.write(servoFirePos);
-      break;
-
-    case RELOAD:
-      servo.write(servoStartPos);
-      analogWrite(PWMController1Pin);
-      analogWrite(PWMController2Pin);
-
-  }
-  
+    analogWrite(motor1Pin, HIGH);
+    analogWrite(motor2Pin, HIGH);
 }
